0ef83c9f94861a40d05e0d3eb859922c2dd286e1
bipinyadav12 <159446360+bipinyadav12@users.noreply.github.com>
2024-11-19 18:13:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+document.getElementById("uForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+const name=document.getElementById('name').value.trim();
+const age=document.getElementById('age').value.trim();
+const agenumber=parseInt(age,10);
+	
+function validateAge(name, age) {
+    return new Promise((resolve, reject) => {
+        if (age > 18) {
+            resolve(`Welcome, ${name}. You can vote.`);
+        } else {
+            reject(`Oh sorry, ${name}. You aren't old enough.`);
+        }
+    });
+}
+validateAge(name, age)
+        .then((message) =>{
+            setTimeout(() => {
+                alert(message)},4000)})
+        .catch((error) => {
+            setTimeout(() => {
+                alert(error)}, 4000);
+        })
+})
+
